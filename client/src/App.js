27d33fd82e3cf94125b42271a69a55d13712f6d2
abubakar-hamad <Abubakarhmdelneel@gmail.com>
2022-07-20 15:06:21 +0200

    getUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
-import { BrowserRouter ,Routes ,Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
 
 function App() {
   return (
-    <>
-  <BrowserRouter>
-  <Navbar />
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-    </Routes>
-  </BrowserRouter>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/hotels" element={<List/>}/>
+        <Route path="/hotels/:id" element={<Hotel/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-
 
 export default App;
